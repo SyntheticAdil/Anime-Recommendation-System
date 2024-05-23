a21debd2bbe948a1fc4d6e70edf3e6229dbd44f3
Adil Ahmed <144491244+SyntheticAdil@users.noreply.github.com>
2024-05-23 23:39:35 +0530

    Add files via upload

---
+++ train_model.py
@@ -0,0 +1,30 @@
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import pickle
+
+
+def train_model(animeData,model_path) :
+    animeData = pd.read_csv("data/AnimeWorld.csv")
+    animeData.reset_index(inplace=True)
+    features = ['Genre', 'Description', 'Studio']
+    for feature in features :
+        animeData[feature] = animeData[feature].fillna('')
+    combined_features = animeData['Genre']+' '+animeData['Description']+' '+animeData['Studio']
+    #Converting the string values to vectors by feature extraction
+    vectoriser = TfidfVectorizer()
+    feature_vectors = vectoriser.fit_transform(combined_features)
+    similarity = cosine_similarity(feature_vectors)
+
+    model = {
+        'vectoriser' : vectoriser,
+        'similarity' : similarity,
+        'animeData' : animeData
+    }
+
+    with open(model_path, 'wb') as f:
+        pickle.dump(model, f)
+
+if __name__ == "__main__" :
+    train_model('data/AnimeWorld.csv', 'models/anime_recommender_model.pkl')
+
